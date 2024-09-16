7c17943a8290b394fd343059c86f3264bd1643d2
vikas chaudhary <vikashkumarchaudahry635@gmail.com>
2024-09-16 16:12:58 +0530

    fixed some issue

--- src/components/createCompany.jsx
+++ src/components/createCompany.jsx
@@ -2,19 +2,24 @@ import { useState } from "react"
 import axios from "axios"
 const CreateCompany = () => {
     const [inpVal, setInpVal] = useState("");
+
+    const [loading, setLoading] = useState(false);
     const submitHandler = async (e) => {
         e.preventDefault()
         const randomCode = Math.floor(1000000000000 + Math.random() * 9000000000000).toString();
         let code = `${randomCode.slice(0, 5)}-${randomCode.slice(5)}`;
         try {
-            const res = await axios.post("http://localhost:5600/api/v1/company/add", { name: inpVal, code });
-            if(res){
-                console.log(res.data);
+            setLoading(true)
+            const res = await axios.post("https://companybackend-code.onrender.com/api/v1/company/add", { name: inpVal, code });
+            if (res) {
+                // console.log(res.data);
                 setInpVal("");
-                
+                setLoading(false);
+
             }
         } catch (error) {
-            console.log(error);
+            // console.log(error);
+            setLoading(false);
 
         }
 
@@ -25,7 +30,7 @@ const CreateCompany = () => {
                 <form >
 
                     <input value={inpVal} onChange={(e) => setInpVal(e.target.value)} />
-                    <button onClick={submitHandler}>Create</button>
+                    <button onClick={submitHandler}>{loading ? "Creating.." : "Create"}</button>
                 </form>
             </>
         </div>
